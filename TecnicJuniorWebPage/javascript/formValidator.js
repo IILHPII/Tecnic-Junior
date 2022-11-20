985b93d05237d95d1cb4a833fecada0ab79bf9d8
IILHPII <l.hernandez071999@gmail.com>
2022-11-20 18:06:38 -0300

    Cambios varios preliminares

--- TecnicJuniorWebPage/javascript/formValidator.js
+++ TecnicJuniorWebPage/javascript/formValidator.js
@@ -6,21 +6,31 @@ const parrrafo = document.getElementById("warning")
 
 formulariojs.addEventListener("submit",e=>{
     e.preventDefault()
+    let warning = ""
+    let entrar = false
+    parrrafo.innerHTML = ""
 
     if(nombre.value.length<6){
-        alert("Nombre no valido")
+        warning += `Nombre no valido<br>`
+        entrar = true
     }
 
-    if(isNaN(telefono.value)){
-        alert("Telefono ingresado no valido")
+    if((isNaN(telefono.value)) || (telefono.value=="")){
+        warning += `Telefono ingresado no valido<br>`
+        entrar = true
     }
 
     let expresion=/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
 
-    if(expresion.test(email.value)){
-        alert("Email valido")
+    if(!expresion.test(email.value)){
+       warning += `Email no valido<br>`
+       entrar = true
+    }
+
+    if(entrar){
+        parrrafo.innerHTML = warning
     }else{
-        alert("La direccion de email es incorrecta")
+        parrrafo.innerHTML = "Enviado"
     }
 
 
